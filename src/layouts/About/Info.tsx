2fdd56f73e943203d4c3719c44be2f63f4dc3c67
Ardian Eka Candra <ardianoption@gmail.com>
2023-02-03 20:31:57 +0700

    fix spacing about info

--- src/layouts/About/Info.tsx
+++ src/layouts/About/Info.tsx
@@ -2,7 +2,7 @@ import { Component } from "solid-js";
 
 export const Info: Component<{}> = (props) => {
   return (
-    <div class="about__info grid grid-cols-2 lg:grid-cols-3 gap-x-2 mb-8 justify-center xl:justify-start">
+    <div class="about__info grid grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-2 lg:gap-y-0  mb-8 justify-center xl:justify-start">
       <div class="about__box bg-white border border-gray-100 py-3 px-2 xl:py-4 xl:px-5 rounded-xl text-center">
         <i class="bx bx-award about__icon text-2xl text-title mb-2"></i>
         <h3 class="about__title text-sm font-medium">Experience</h3>
