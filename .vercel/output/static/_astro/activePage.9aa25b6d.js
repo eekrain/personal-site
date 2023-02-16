68b5e7e1d2744a9ceb9981ac1c6dd0543a5d437c
Ardian Eka Candra <ardianoption@gmail.com>
2023-02-16 18:34:35 +0700

    add ignore .vercel

--- .vercel/output/static/_astro/activePage.9aa25b6d.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./web.56bfa28a.js";const[t,s]=a("home");export{t as a,s};
--- .vercel/output/static/_astro/index.28058b83.js
+++
@@ -1,1 +0,0 @@
-import{A as e,T as o,t as r,r as l,a as p}from"./index.1772c172.js";import"./web.56bfa28a.js";export{e as ActionType,o as Toaster,r as default,l as resolveValue,p as toast};
